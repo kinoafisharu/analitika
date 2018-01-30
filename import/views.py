f8d8a107094af9475609cd98f0340ed7cbdf388b
gikta <gikta2@gmail.com>
2018-01-30 15:25:26 +0300

    template view

--- import/views.py
+++ import/views.py
@@ -3,9 +3,10 @@ from .resource import *
 from django.contrib import messages
 # Create your views here.
 from .import_export_views import *
-
+from .models import *
 
 def home(request):
+    offers = Offers.objects.all()
     if request.POST:
         file = request.FILES['file']
         uploading_file = UploadingProducts({'file': file})
