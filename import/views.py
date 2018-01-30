88eddaf258d7c8bbbe253319dc8638883fd3180a
gikta <gikta2@gmail.com>
2018-01-30 15:36:33 +0300

    fix file take

--- import/views.py
+++ import/views.py
@@ -4,14 +4,18 @@ from django.contrib import messages
 # Create your views here.
 from .import_export_views import *
 from .models import *
+from django.utils.datastructures import MultiValueDictKeyError
 
 def home(request):
     offers = Offers.objects.all()
     if request.POST:
-        file = request.FILES['file']
-        uploading_file = UploadingProducts({'file': file})
-        if uploading_file:
-            messages.success(request, "Загружено")
-        else:
-            messages.error(request, "Ошибка")
+        try:
+            file = request.FILES['file']
+            uploading_file = UploadingProducts({'file': file})
+            if uploading_file:
+                messages.success(request, "Загружено")
+            else:
+                messages.error(request, "Ошибка")
+        except MultiValueDictKeyError:
+            messages.error(request, "Выберите файл!")
     return render(request, 'home.html', locals())
